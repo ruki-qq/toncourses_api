11f65bfa147a39a45100f4935938804d28f8b6d1
ruki-qq <ruki@duck.com>
2024-04-27 12:52:40 +0300

    Project boilerplate

---
+++ app/main.py
@@ -0,0 +1,7 @@
+# from fastapi import FastAPI
+#
+# from api import router_v1
+# from core.config import settings
+#
+# app = FastAPI()
+# app.include_router(router_v1, prefix=settings.api_v1_prefix)
