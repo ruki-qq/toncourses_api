62fd195e4b7822abfa70655f76c51991d6e6bd44
ruki-qq <ruki@duck.com>
2024-04-27 17:23:02 +0300

    Bound users and courses for authors and students

--- app/models/course.py
+++ app/models/course.py
@@ -1,14 +1,23 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey, SmallInteger, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy_utils import URLType
+
 from .base import Base
 from .mixins import TimestampModelMixin
 
-from sqlalchemy import SmallInteger, String, Text
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy_utils import URLType
+if TYPE_CHECKING:
+    from .user import User
+    from .user_course_association import UserCourseAssociation
 
 
 class Course(TimestampModelMixin, Base):
     title: Mapped[str] = mapped_column(String(100))
     description: Mapped[str] = mapped_column(Text, default="", server_default="")
+    author_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    author: Mapped["User"] = relationship(back_populates="created_courses")
+    users: Mapped[list["UserCourseAssociation"]] = relationship(back_populates="user")
     rating: Mapped[float] = mapped_column(SmallInteger, default=0, server_default="0")
     title_pic: Mapped[str | None] = mapped_column(URLType)
 
--- app/models/user.py
+++ app/models/user.py
@@ -1,21 +1,24 @@
-from datetime import datetime
+from typing import TYPE_CHECKING
 
-from sqlalchemy import String, func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy_utils import EmailType
 
 from .base import Base
+from .mixins import TimestampModelMixin
+
+if TYPE_CHECKING:
+    from .course import Course
+    from .user_course_association import UserCourseAssociation
 
 
-class User(Base):
+class User(TimestampModelMixin, Base):
     username: Mapped[str] = mapped_column(String(16), unique=True)
     email: Mapped[str | None] = mapped_column(EmailType, unique=True)
     first_name: Mapped[str] = mapped_column(String(64))
     last_name: Mapped[str] = mapped_column(String(64))
-    register_at: Mapped[datetime] = mapped_column(
-        default=datetime.now,
-        server_default=func.now(),
-    )
+    created_courses: Mapped[list["Course"]] = relationship(back_populates="author")
+    courses: Mapped[list["UserCourseAssociation"]] = relationship(back_populates="user")
 
     def __str__(self):
         return f"{self.__class__.__name__} id={self.id} username={self.username}"
