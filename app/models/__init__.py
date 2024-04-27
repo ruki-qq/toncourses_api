b69e86966aa1449107689baf0a10c1507dffb094
ruki-qq <ruki@duck.com>
2024-04-27 17:29:26 +0300

    Add migrations for user and course models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,11 @@
 __all__ = (
     "Base",
+    "Course",
     "User",
+    "UserCourseAssociation",
 )
 
 from .base import Base
+from .course import Course
 from .user import User
+from .user_course_association import UserCourseAssociation
