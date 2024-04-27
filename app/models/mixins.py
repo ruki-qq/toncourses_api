93a46a19b938b58bf02ae3a5cd34496ef8b3e250
ruki-qq <ruki@duck.com>
2024-04-27 17:22:22 +0300

    Add user relation mixin

--- app/models/mixins.py
+++ app/models/mixins.py
@@ -1,7 +1,11 @@
 from datetime import datetime
+from typing import TYPE_CHECKING
 
-from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, func
+from sqlalchemy.orm import Mapped, declared_attr, mapped_column, relationship
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class TimestampModelMixin:
@@ -9,3 +13,23 @@ class TimestampModelMixin:
         default=datetime.now, server_default=func.now()
     )
     updated_at: Mapped[datetime | None] = mapped_column(onupdate=datetime.now)
+
+
+class UserRelationMixin:
+    _user_id_nullable: bool = False
+    _user_id_unique: bool = False
+    _user_back_populates: str | None = None
+
+    @declared_attr
+    def user_id(self) -> Mapped[int]:
+        return mapped_column(
+            ForeignKey("users.id"),
+            nullable=self._user_id_nullable,
+            unique=self._user_id_unique,
+        )
+
+    @declared_attr
+    def user(self) -> Mapped["User"]:
+        return relationship(
+            back_populates=self._user_back_populates,
+        )
